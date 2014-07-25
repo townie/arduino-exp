9a632b1dfe34e45158807346bac59bc282b3c1ba
Keith Webber <kwebber@backupify.com>
2014-07-25 00:01:14 -0400

    cleaned up some messy code

--- sleep_light_sensor.rb
+++ sleep_light_sensor.rb
@@ -1,28 +1,26 @@
+
+
 int red = 12;
 int green = 2;
 int lightSensor = A0;
 
-// the setup routine runs once when you press reset:
 void setup() {
-  // initialize the digital pin as an output.
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
-  pinMode(lightSensor, OUTPUT);
+  pinMode(lightSensor, INPUT);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-
-  delay(1000);
   int lightDensity = analogRead(lightSensor);
+  delay(1000);
 
-
-  if (lightDensity >= 500) {
-    digitalWrite(green, HIGH);
-    digitalWrite(red, LOW);
-  }
-  else {
+  if (lightDensity >= 20) {
     digitalWrite(red, HIGH);
     digitalWrite(green, LOW);
   }
+  else {
+    digitalWrite(green, HIGH);
+    digitalWrite(red, LOW);
+  }
 }
+
